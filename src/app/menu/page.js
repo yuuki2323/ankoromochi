1b04c45000a97c582ff11a29664462a800cbe29c
yuuki2323 <yuuki.work23@gmail.com>
2024-01-04 22:29:32 +0900

    a

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../toppageCpmponent/Header'
 import SalonFooter from '../toppageCpmponent/SalonFooter'
-
+import Image from 'next/image'
 const page = () => {
   return (
     <>
     <Header/>
-    <div>page</div>
+    <div>
+      <h2>オイルトリートメント</h2>
+      <Image src="/oil.jpg" alt="oil" width={336} height={280}/>
+    </div>
     <SalonFooter/>
     </>
   )
