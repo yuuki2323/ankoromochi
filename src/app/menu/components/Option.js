33eacc2d5d81f52118ab9dcad8a3abf5e5a23eb2
yuuki2323 <yuuki.work23@gmail.com>
2024-01-09 09:14:31 +0900

    1

--- src/app/menu/components/Option.js
+++ src/app/menu/components/Option.js
@@ -1,31 +1,53 @@
-
-import React from 'react'
+import React from "react";
 
 const Option = () => {
   return (
     <div className="w-11/12 mx-auto bg-slate-50 mb-8">
-    <div className="w-11/12 mx-auto">
-      <h3 className="text-center">オプションメニュー</h3>
+      <div className="w-11/12 mx-auto">
+        <h3 className="text-center">オプションメニュー</h3>
 
-
-      <ul>
-        <p>クリームヘッドスパ </p>
-        <li className="flex justify-between border-b" >
-          <p>30分</p>
-          <p>&yen;6000</p>
-        </li>
-        <li className="flex justify-between border-b" >
-          <div>
-          <p>30分</p>
-          </div>
-
-          <p>&yen;6000</p>
-        </li>
-
-      </ul>
+        <ul>
+          <p>クリームヘッドスパ </p>
+          <li className="flex justify-between border-b">
+            <p>30分</p>
+            <p>&yen;6000</p>
+          </li>
+          <p>リフレクソロジー</p>
+          <li className="flex justify-between border-b">
+            <p>30分</p>
+            <p>&yen;6000</p>
+          </li>
+          <li className="flex justify-between border-b">
+            <p>60分</p>
+            <p>&yen;10,000</p>
+          </li>
+          <p>フェイシャル</p>
+          <li className="flex justify-between border-b">
+            <p>30分</p>
+            <div className=" text-right">
+              <p>&yen;5,000</p>
+              <p>パック別途&yen;2,000</p>
+            </div>
+          </li>
+          <p>ドライヘッドスパ</p>
+          <li className="flex justify-between border-b">
+            <p>10分</p>
+            <p>&yen;2,000</p>
+          </li>
+          <p>パウエルトリートメント</p>
+          <li className="flex justify-between border-b">
+            <p>10分</p>
+            <p>&yen;2,000</p>
+          </li>
+          <p>ハンドトリートメント</p>
+          <li className="flex justify-between border-b">
+            <p>10分</p>
+            <p>&yen;2,000</p>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default Option;
