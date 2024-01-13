352a74be551dad502beeb1d5fc6fd3062eeeadb1
yuuki2323 <yuuki.work23@gmail.com>
2024-01-13 23:57:23 +0900

    1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,11 @@
 
 import {Kosugi_Maru} from 'next/font/google'
 import './globals.css'
-
+import Head from 'next/head'
 
 const Kosugi = Kosugi_Maru({ subsets: ['latin'],weight:"400"})
 
-export const metadata = {
+ const meta = {
   title: '癒し処 あんころもち',
   description: '癒し処 あんころもち',
 }
@@ -13,6 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description}/>
+      </Head>
       <body className={`Kosugi.className text-brown-900`}>{children}</body>
     </html>
   )
