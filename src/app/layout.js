b5615f62298efac1c16e1a4e7fce1c0de2bc3186
yuuki2323 <yuuki.work23@gmail.com>
2024-01-16 13:48:19 +0900

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ const Kosugi = Kosugi_Maru({ subsets: ['latin'],weight:"400"})
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <Head key="head">
+      <Head>
         <link rel="icon" href="/favicon.ico"/>
         <title>神戸・三宮のリラクゼーションサロン 癒し処 あんころもち</title>
         <meta name="description" content="神戸・三宮のリラクゼーション癒し処あんころもちでは、メンズもご利用可能なオイルリンパマッサージ・ストレッチ・揉みほぐし・ヘッドスパ等様々なメニューを取り揃え！ワンフロア一戸のマンション・シャワー完備の隠れ家的サロン。"/>
