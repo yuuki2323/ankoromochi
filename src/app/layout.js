67c97bdb4c95565dfa7613f88e9e8ca0d5dbbd56
yuuki2323 <yuuki.work23@gmail.com>
2024-01-16 14:35:01 +0900

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,19 @@
 
 import {Kosugi_Maru} from 'next/font/google'
 import './globals.css'
-import Headcom from './Headcom'
+
 const Kosugi = Kosugi_Maru({ subsets: ['latin'],weight:"400"})
+export const metadata = {
+  title: "神戸・三宮のリラクゼーションサロン 癒し処 あんころもち",
+  description: "神戸・三宮のリラクゼーション癒し処あんころもちでは、メンズもご利用可能なオイルリンパマッサージ・ストレッチ・揉みほぐし・ヘッドスパ等様々なメニューを取り揃え！ワンフロア一戸のマンション・シャワー完備の隠れ家的サロン。",
+
+};
 
 
 export default function RootLayout({ children }) {
 
   return (
     <html lang="ja">
-      <Headcom/>
       <body className={`${Kosugi.className} text-brown-900`}>{children}</body>
     </html>
   )
