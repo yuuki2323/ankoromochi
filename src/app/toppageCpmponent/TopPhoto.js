34160f16a250a42b1923820ec7054c6afd780059
yuuki2323 <yuuki.work23@gmail.com>
2024-01-30 20:20:35 +0900

    A

--- src/app/toppageCpmponent/TopPhoto.js
+++ src/app/toppageCpmponent/TopPhoto.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import SumahoPhoto from './topUI/SumahoPhoto'
-import PcPhoto from './topUI/Pcphoto'
+import PcPhoto from './topUI/PcPhoto'
 const TopPhoto = () => {
  
   return (
