b2dd6d61464b424e4f72cb380f2b7727e85d0a7a
yuuki2323 <yuuki.work23@gmail.com>
2024-04-17 21:32:38 +0900

    q

--- src/app/menu/components/MenuOption.js
+++ src/app/menu/components/MenuOption.js
@@ -9,12 +9,6 @@ const MenuOption = () => {
           <h3 className="text-center py-1 text-lg  lg:text-xl xl:text-2xl">オプションメニュー</h3>
 
           <ul>
-            <p >・クリームヘッドスパ </p>
-            <p className="text-xs">お客様のお好みに合わせて5種類の中からお選び頂いたクリームを使う頭皮・髪・首までのトリートメント。</p>
-            <li className="flex justify-between border-b mb-2">
-              <p>30分</p>
-              <p>&yen;6,000</p>
-            </li>
             <p>・リフレクソロジー</p>
             <p className="text-xs">台湾式の足ツボのメニュー。</p>
             <li className="flex justify-between border-b ">
@@ -34,23 +28,32 @@ const MenuOption = () => {
                 <p>パック別途&yen;2,000</p>
               </div>
             </li>
-            <p>・ドライヘッドスパ</p>
-            <p className="text-xs">クリームを使用せずに行うヘッドスパ。頭皮の筋肉をほぐします。</p>
-            <li className="flex justify-between border-b mb-2">
-              <p>10分</p>
-              <p>&yen;2,000</p>
-            </li>
             <p>・バウエルトリートメント</p>
             <p className="text-xs">オイルを使用し固くなった大腸、小腸、骨盤まわりを揉みほぐします。</p>
             <li className="flex justify-between border-b mb-2">
               <p>10分</p>
               <p>&yen;2,000</p>
             </li>
-            <p>ハンドトリートメント</p>
-            <p className="text-xs">指先や手のひらの筋肉やコリをほぐし、末端を刺激することで血流を促します。</p>
+            <p >・クリームヘッドスパ </p>
+            <p className="text-xs">お客様のお好みに合わせて5種類の中からお選び頂いたクリームを使う頭皮・髪・首までのトリートメント。</p>
             <li className="flex justify-between border-b mb-2">
-              <p>10分</p>
-              <p>&yen;2,000</p>
+              <p>30分</p>
+              <p>&yen;6,000</p>
+            </li>
+            <p>炭酸ヘッドスパ</p>
+            <li className="flex justify-between border-b mb-2">
+              <p>30分</p>
+              <p>&yen;5,000</p>
+            </li>
+            <p>スカルプヘッドスパ</p>
+            <li className="flex justify-between border-b mb-2">
+              <p>30分</p>
+              <p>&yen;6,000</p>
+            </li>
+            <p>ドライヘッドスパ</p>
+            <li className="flex justify-between border-b mb-2">
+              <p>30分</p>
+              <p>&yen;5,000</p>
             </li>
             <p>各延長</p>
             <li className="flex justify-between border-b">
